35d6a25ee271ea3e8685cdf50c6e1640e3938a3d
trivedi-ashutosh2013 <trivedi.ashutosh2013@gmail.com>
2026-06-26 09:14:36 +0530

    Add loan approval pipeline to trigger_pipeline.py

--- trigger_pipeline.py
+++ trigger_pipeline.py
@@ -2,6 +2,14 @@ import boto3
 
 sm = boto3.client("sagemaker")
 
+pipeline = Pipeline(
+    name="loan-approval-pipeline",
+    steps=[...],
+    sagemaker_session=session
+)
+
+pipeline.upsert(role_arn=role)
+
 response = sm.start_pipeline_execution(
     PipelineName="loan-approval-pipeline"
 )
