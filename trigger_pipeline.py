90130342455baa98b1403bc14989421c39ae3a9b
trivedi-ashutosh2013 <trivedi.ashutosh2013@gmail.com>
2026-06-26 09:17:17 +0530

    Import additional SageMaker workflow modules

--- trigger_pipeline.py
+++ trigger_pipeline.py
@@ -1,5 +1,10 @@
 import boto3
-
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.session import Session
+from sagemaker.workflow.steps import TrainingStep
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.workflow.pipeline_context import PipelineSession
+from sagemaker.sklearn.estimator import SKLearn
 sm = boto3.client("sagemaker")
 
 pipeline = Pipeline(
